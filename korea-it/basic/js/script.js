f088728f7fef32be445ef66b7e824436842aedad
tvvmvn <tvvmvn@gmail.com>
2022-04-14 17:52:58 +0900

    4/14

--- korea-it/basic/js/script.js
+++ korea-it/basic/js/script.js
@@ -1,3 +1,5 @@
+// ** JavaScript Lecture
+
 // 4/12 Tuesday
 
 // # Basic
@@ -20,7 +22,6 @@
 
 // # Object and Array
 
-
 // 4/13 Wendsday
 
 // # Function and Method
@@ -41,6 +42,32 @@
 // make array with uppercase letter. 
 // make function returning adult or children with age parameter typed string or number.
 // get the cars array only on sale.
+
+
+// 4/14 Thursday
+
+// # JavaScript DOM Basic
+
+// # Selector
+// Differences between NodeList and HTMLCollection
+// getElementById, getElementsByClassName (HTMLCollection)
+// querySelector, querySelectorAll (NodeList)
+// parentElement, nextElementSibling, children, childNodes
+// firstChild, firstElementChild, lastChild
+
+// # Attribute
+// setAttribute, getAttribute, accessing attribute(input.value ...)
+// style, classList(add, remove, toggle)
+
+// # Print
+// innerHTML, textContent, createElement, appendChild
+
+// # Event 
+// addEventListener and onclick, this, e.target
+// load, click, scroll, resize, ...
+
+
+
 
 
 
