4aa805c37d52dcb8f7964b6f62364adee3e26afa
tvvmvn <tvvmvn@gmail.com>
2022-05-03 17:58:02 +0900

    5/3

--- node-server/csr/public/js/index.js
+++ node-server/csr/public/js/index.js
@@ -1,35 +1,116 @@
+
+router()
+
 window.addEventListener('popstate', () => {
   router()
 })
+
 document.body.addEventListener('click', e => {
   if (e.target.matches('[data-link]')) {
-    e.preventDefault()
-    navigateTo(e.target.getAttribute('href'))
+    e.preventDefault();
+    
+    const url = e.target.getAttribute('href')
+    history.pushState(null, null, url);
+    router();
   }
 })
-document.addEventListener('DOMContentLoaded', () => {
-  router()
-})
-
-function navigateTo(url) {
-  history.pushState(null, null, url);
-  router();
-}
 
 function router() {
-  console.log(location.pathname)
-
   const routes = [
-    { path: '/', view: () => console.log('Home') },
-    { path: '/posts', view: () => console.log('Posts') },
-    { path: '/contact', view: () => console.log('Contact') },
+    { path: '/', view: () => Home({ fruit: 'Apple' }) },
+    { path: '/posts', view: Posts },
+    { path: '/contact', view: Contact },
   ]
 
-  const route = routes.filter(route => location.pathname === route.path)
+  const route = routes.filter(route => location.pathname === route.path);
 
-  if (!route[0]) {
-    return console.log('Not Found')
+  const element = {
+    type: 'div',
+    props: { 'id': 'root' },
+    children: [Nav(), route[0].view()]
   }
 
-  route[0].view()
+  document.getElementById('root').replaceWith(createElement(element))
+}
+
+function Nav() {
+  return {
+    type: 'nav',
+    props: { 'class': 'nav' },
+    children: [
+      { type: 'ul', props: {}, children: [
+        { type: 'li', props: {}, children: [ { type: 'a', props: { 'href': '/', 'class': 'nav__link', 'data-link': '' }, children: ['Home'] }
+        ] },
+        { type: 'li', props: {}, children: [
+          { type: 'a', props: { 'href': '/posts', 'class': 'nav__link', 'data-link': '' }, children: ['Posts'] }
+        ] },
+        { type: 'li', props: {}, children: [
+          { type: 'a', props: { 'href': '/contact', 'class': 'nav__link', 'data-link': '' }, children: ['Contact'] },
+        ] },
+      ] }
+    ]
+  }
+}
+
+function Home(props) { 
+
+  function handleClick() {}
+  
+  return {
+    type: 'div',
+    props: {},
+    children: [
+      { type: 'h1', props: {}, children: ['Home'] },
+      { type: 'p', props: {}, children: [props.fruit] },
+      { type: 'button', props: { 'onClick': handleClick }, children: ['button'] },
+    ]
+  }
+}
+
+function Posts() {
+  console.log('Posts Loaded!')
+
+  // fetch('http://localhost:3000')
+  // .then(res => res.text())
+  // .then(result => console.log(result))
+    
+  return {
+    type: 'h1',
+    props: {},
+    children: ['Posts']
+  }
+}
+
+function Contact() {
+  return {
+    type: 'h1',
+    props: {},
+    children: ['Contact']
+  }
+}
+
+function createElement(node) {
+  if (typeof node === 'string') {
+    return document.createTextNode(node)
+  }
+
+  const $el = document.createElement(node.type);
+
+  Object.keys(node.props).map(prop => {
+    if (prop.match(/^on/)) {
+      $el.addEventListener(prop.slice(2).toLocaleLowerCase(), node.props[prop]) 
+    } else if (typeof node.props[prop] === 'boolean') {
+      if (node.props[prop]) {
+        $el.setAttribute(prop, '')
+      } 
+    } else {
+      $el.setAttribute(prop, node.props[prop])
+    }
+  })
+  
+  node.children.map(c => {
+    $el.appendChild(createElement(c))
+  })
+
+  return $el;
 }
--- node-server/ssr/routes/index.js
+++ node-server/ssr/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs')
+var fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,28 +10,65 @@ router.get('/', function(req, res, next) {
   })
 });
 
-router.get('/new', function(req, res, next) {
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+// Create
+router.get('/create', function(req, res, next) {
   res.render('blog_form', { title: 'Blog Form' });
 });
 
-router.post('/new', function(req, res, next) {
+router.post('/create', function(req, res, next) {
   fs.appendFile(`data/${req.body.title}.txt`, req.body.content, (err) => {
-    if (err) throw err;
+    if (err) {
+      return console.error(err)
+    }
     res.redirect('/');
   })
 });
 
-router.get('/post/:postId', (req, res, next) => {
+// Read
+router.get('/p/:postId', (req, res, next) => {
     fs.readFile(`data/${req.params.postId}`, (err, content) => {
-      if (err) throw err;
+      if (err) {
+        return console.error(err)
+      }
       const post = { title: req.params.postId, content }
       res.render('blog_detail', { title: 'Blog', post })
     })
 })
 
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About' });
-});
+// Update
+router.get('/p/:postId/update', (req, res, next) => {
+  fs.readFile(`data/${req.params.postId}`, (err, content) => {
+    if (err) {
+      return console.error(err)
+    }
+    const post = { title: req.params.postId, content }
+    res.render('blog_form', { title: 'Blog', post })
+  })
+})
 
+router.post('/p/:postId/update', (req, res, next) => {
+  fs.rename(`data/${req.body.ex_title}`, `data/${req.body.title}.txt`, (err, content) => {
+    if (err) { return console.error(err) }
+
+    fs.writeFile(`data/${req.body.title}.txt`, req.body.content, (err) => {
+      if (err) { return console.error(err) }
+      res.redirect(`/p/${req.body.title}.txt`);
+    })
+  })
+})
+
+// Delete
+router.post('/p/:postId/delete', (req, res, next) => {
+    fs.unlink(`data/${req.params.postId}`, (err) => {
+      if (err) {
+        return console.error(err)
+      }
+      res.json({ message: 'success' })
+    })
+})
 
 module.exports = router;
