7f3886f232235a4b00e97deb9ca0c157724f0e18
tvvmvn <tvvmvn@gmail.com>
2022-10-14 19:41:38 +0900

    10/14

--- basic/react-basic/React.md
+++ basic/react-basic/React.md
@@ -7,29 +7,20 @@ Compose them to make complex UIs.
 - Learn Once, Write Anywhere
 power mobile apps using ReactNative
 
+
 # Basic
 - JSX
 - Component
-Composition
-- props
+Composition, props
 - Hook
 useState, useEffect, useRef, useContext
-- Router
-ErrorBoundary
-- Router/Auth
-- Form
 - Event
+Button, Form
+- Router Basic
+- Router/Auth
 - Fetch data
-useEffect, Suspense
-
+useEffect
 
 # Example
-- Favorite
-- Form
-- Carousel
-- Fetch data
-
-
-# React Project
-Todo list
+- Todo list
 
--- basic/react-basic/src/App.js
+++ basic/react-basic/src/App.js
@@ -5,28 +5,28 @@ import './App.css';
 export default App;
 
 // Declarative VS Imperative
-function App() {
-  const [items, setItems] = useState([
-    Math.random()
-  ]);
+// function App() {
+//   const [items, setItems] = useState([
+//     Math.random()
+//   ]);
 
 
-  function handleClick() {
-    setItems([...items, Math.random()])
-  }
+//   function handleClick() {
+//     setItems([...items, Math.random()])
+//   }
 
-  return (
-    <>
-      <h2>Declarative</h2>
-      <ul>
-        {items.map(item => <li key={item}>{item}</li>)}
-      </ul>
-      <button onClick={handleClick}>Add</button>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <h2>Declarative</h2>
+//       <ul>
+//         {items.map(item => <li key={item}>{item}</li>)}
+//       </ul>
+//       <button onClick={handleClick}>Add</button>
+//     </>
+//   )
+// }
 
-// # JSX
+// # JSX Syntax
 // A Syntax extension to JavaScript
 // Declarative like HTML
 // BABEL compile JSX to JavaScript Object.
@@ -35,13 +35,11 @@ function App() {
 //   return <h1>Hello React</h1>
 // }
 
-// # Without JSX
+// Without JSX
 // function App() {
 //   const h1 = React.createElement('h1', 'null', 'Hello React');
 //   return h1;
 // }
-
-// # JSX Syntax
 
 // ### JSX elements must be wrapped in one element.
 // function App() {
@@ -65,8 +63,8 @@ function App() {
 
 // ### React.Fragment
 // function App() {
-
 //   return (
+// DOM에 별도의 노드를 추가하지 않고 자식을 그룹화 할 수 있다
 //     <React.Fragment>
 //       <h2>React Fragment</h2>
 //       <div>Foo</div>
@@ -87,20 +85,6 @@ function App() {
 //   )
 // }
 
-// ### JSX Self-closing elements
-// function App() {
-//   return (
-//     <>
-//       <h2>JSX Self-closing elements</h2>
-//       <ul>
-//         <li>Foo</li>
-//         <li><img src="/" alt="Bar" /></li>
-//         <li><input type="text" placeholder="Baz" /></li>
-//       </ul>
-//     </>  
-//   )
-// }
-
 // ### JSX Attribute
 // function App() {
 //   return (
@@ -115,13 +99,13 @@ function App() {
 //   )
 // }
 
-// ### JSX inline style
+// ### JSX inline style attribute
 // pass the style object to style attribute
 
 // function App() {
 //   return (
 //     <>
-//       <h1>JSX Styles</h1>
+//       <h1>JSX inline Styles</h1>
 //       <ul style={{border: "1px solid"}}>
 //         <li style={{textDecoration: "underline"}}>Foo</li>
 //         <li>Bar</li>
@@ -139,44 +123,62 @@ function App() {
 //       <h2>Write expressions in JSX</h2>
 //       <div>{foo}</div>
 //       <div>{"Ba" + "r"}</div>
-//       <div>{new String("Baz").valueOf()}</div>
+//       <div>{"Baz".valueOf()}</div>
 //     </>
 //   )
 // }
 
-// ### JSX Condition
-// Logical && (AND): expr1 && expr2
-// Logical || (OR): expr1 || expr2
-// Ternary: condition ? expr if true : expr if false
-
+// Q. Complete DOM with JS Object.
 // function App() {
+//   const dutchBeer = "Heineken";
+//   const irishBeer = "Guinness";
+//   const japaneseBeer = "Asahi";
+
+//   const soldOut = {
+//     textDecoration: "line-through",
+//     color: "#ddd"
+//   };
+
 //   return (
 //     <>
-//       <h2>Logical &amp;&amp; (AND)</h2>
-//       <div>{true && "Foo"}</div>
-//       <div>{!"" && "Bar"}</div>
-//       <div>{2 && "Bar"}</div>
+//       <h2>Beers</h2>
+//       <ul>
+//         <li>{dutchBeer}</li>
+//         <li style={soldOut}>{irishBeer}</li>
+//         <li>{japaneseBeer}</li>
+//       </ul>
 //     </>  
 //   )
 // }
 
-// function App() {
-//   return (
-//     <>
-//       <h2>Logical || (OR)</h2>
-//       <div>{false || "Foo"}</div>
-//       <div>{"" || "Bar"}</div>
-//       <div>{0 || "Baz"}</div>
-//     </>
-//   )
-// }
+// ### JSX Condition
+// Logical operator, Ternary operator
+// && (AND): expr1 && expr2
+// || (OR): expr1 || expr2
+// ! (NOT): !expr
+// Ternary: condition ? expr if true : expr if false
 
 // function App() {
+
+//   // &&
+//   console.log(true && 1); // 1
+//   console.log(1 && 0); // 0
+//   console.log(0 && 1); // 0
+//   console.log(0 && false); // 0
+
+//   // ||
+//   console.log(1 || true); // 1
+//   console.log(1 || 0); // 1
+//   console.log(0 || 1); // 1
+//   console.log(false || 0); // 0
+
 //   return (
 //     <>
-//       <h2>Ternary</h2>
-//       <div>{true ? "Foo" : "Bar"}</div>  
-//     </>
+//       <h2>JSX condition</h2>
+//       <div>{true && "Foo"}</div>
+//       <div>{false || "Bar"}</div>
+//       <div>{!true ? "" : "Baz"}</div>
+//     </>  
 //   )
 // }
 
@@ -215,27 +217,6 @@ function App() {
 
 // Q. Complete DOM with JS Object.
 // function App() {
-//   const dutchBeer = "Heineken";
-//   const irishBeer = "Guinness";
-//   const japaneseBeer = "Asahi";
-
-//   const soldOut = {
-//     textDecoration: "line-through",
-//   };
-
-//   return (
-//     <>
-//       <h2>Beers</h2>
-//       <ul>
-//         <li>{dutchBeer}</li>
-//         <li style={soldOut}>{irishBeer}</li>
-//         <li>{japaneseBeer}</li>
-//       </ul>
-//     </>  
-//   )
-// }
-
-// function App() {
 //   const beers = [
 //     {name: "Heineken", origin: "Netherlands"},
 //     {name: "Guinness", origin: "Ireland"},
@@ -255,3 +236,258 @@ function App() {
 //     </>
 //   )
 // }
+
+// # COMPONENTS
+// independent and reusable pieces to build completex UIs.
+// 2 types of components: function components, class components
+
+// ### Component Basic
+// Components must starts with Uppercase letter.
+// function components is basically function
+// function App() {
+//   // expression, code
+//   console.log("App Loaded!");
+
+//   // return VirtualDOM
+//   return <h1>App</h1>
+// }
+
+/// ### Components Composition
+// function Foo() {
+//   return <li>Foo</li>
+// }
+// function Bar() {
+//   return <li>Bar</li>
+// }
+// function Baz() {
+//   return <li>Baz</li>
+// }
+
+// function App() {
+
+//   return (
+//     <ul>
+//       <Foo />
+//       <Bar />
+//       <Baz />
+//     </ul>  
+//   )
+// }
+
+// Q. Components composition 
+// function DutchBeer() {
+//   return (
+//     <tr>
+//       <td>Heineken</td>
+//       <td>Netherlands</td>
+//       <td>Yes</td>
+//     </tr>
+//   )
+// }
+
+// function IrishBeer() {
+//   return (
+//     <tr>
+//       <td>Guinness</td>
+//       <td>Ireland</td>
+//       <td>No</td>
+//     </tr>  
+//   )
+// }
+
+// function JapaneseBeer() {
+//   return (
+//     <tr>
+//       <td>Asahi</td>
+//       <td>Japan</td>
+//       <td>Yes</td>
+//     </tr>  
+//   )
+// }
+
+// function App() {
+//   return (
+//     <>
+//       <h1>Beers</h1>
+//       <table>        
+//         <thead>
+//           <tr>
+//             <th>Brand</th>
+//             <th>Origin</th>
+//             <th>Available</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           <DutchBeer />
+//           <IrishBeer />
+//           <JapaneseBeer />
+//         </tbody>
+//       </table>
+//     </>  
+//   )
+// }
+
+// ### props
+// parameters to pass components
+
+// Basic
+// function C(props) {
+//   console.log(props);
+
+//   const foo = props.foo;
+//   const bar = props.bar;
+//   const baz = props.baz;
+
+//   return (
+//     <>
+//       <li>{foo}</li>  
+//       <li>{bar}</li>  
+//       <li>{baz}</li>
+//     </>
+//   )
+// }
+
+// function App() {
+
+//   return (
+//     <ul>
+//       <C foo="Foo" bar="Bar" baz="Baz" />
+//     </ul>  
+//   )
+// }
+
+// Q. Basic props
+// function Beer(props) {
+//   const name = props.name;
+//   const origin = props.origin;
+//   const available = props.available;
+
+//   return (
+//     <ul>
+//       <li>Name: {name}</li>
+//       <li>Origin: {origin}</li>
+//       <li>Available: {available ? "Yes" : "No"}</li>
+//     </ul>
+//   )
+// }
+
+// function App() {
+//   return (
+//     <>
+//       <h1>Beers</h1>
+//       <Beer
+//         name="Guinness"
+//         origin="Ireland"
+//         available={false}
+//       />
+//     </>
+//   )
+// }
+
+// Object props
+// function C(props) {
+//   console.log(props);
+
+//   const o = props.o;
+
+//   return (
+//     <ul>
+//       <li>{o.prop1}</li>
+//       <li>{o.prop2}</li>
+//       <li>{o.prop3}</li>
+//     </ul>  
+//   )
+// }
+
+// const o = {prop1: "Foo", prop2: "Bar", prop3: "Baz"}
+
+// function App() {
+
+//   return (
+//     <>
+//       <h1>App</h1>
+//       <C o={o} />  
+//     </>
+//   )
+// }
+
+// Q. Object props
+// function Beer(props) {
+//   const beer = props.beer;
+
+//   return (
+//     <ul>
+//       <li>Name: {beer.name}</li>
+//       <li>Origin: {beer.origin}</li>
+//       <li>Available: {beer.available ? "Yes" : "No"}</li>
+//     </ul>  
+//   )
+// }
+
+// const irishBeer = {name: "Guinness", origin: "Ireland", available: false};
+
+// function App() {
+
+//   return (
+//     <>
+//       <h1>Beers</h1>
+//       <Beer beer={irishBeer} />  
+//     </>
+//   )
+// }
+
+// ### Component reusage
+// const arr = ["Foo", "Bar", "Baz"];
+
+// function C(props) {
+//   const item = props.item;
+
+//   return <li>{item}</li>
+// }
+
+// function App() {
+
+//   const list = arr.map((item, index) => (
+//     <C key={index} item={item} />  
+//   ))
+
+//   return (
+//     <ul>
+//       {list}
+//     </ul>  
+//   )
+// }
+
+// Q. Reuse components.
+// const beers = [
+//   {name: "Heineken", origin: "Netherlands"},
+//   {name: "Guinness", origin: "Ireland"},
+//   {name: "Kloud", origin: "S.Korea"},
+// ]
+
+// function Beer(props) {
+//   const beer = props.beer;
+
+//   return <li>{beer.name}, {beer.origin}</li>
+// }
+
+// function App() {
+
+//   const list = beers.map((beer, index) => (
+//     <Beer 
+//       key={index} 
+//       beer={beer} 
+//     />  
+//   ))
+
+//   return (
+//     <>
+//       <h1>Beers</h1>
+//       <ul>
+//         {list}
+//       </ul>
+//     </>  
+//   )
+// }
+
+// ### children props
