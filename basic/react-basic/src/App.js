f587ca64ecc8b0f56d15658785c523e6807489f3
tvvmvn <tvvmvn@gmail.com>
2022-11-17 18:53:24 +0900

    11/17

--- basic/react-basic/src/App.js
+++ basic/react-basic/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef, useContext, createContext} from 'react';
+import React, {useState, useEffect, useRef, useContext, createContext, Children} from 'react';
 import {BrowserRouter as Router, Routes, Route, Link, Outlet, useParams} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
@@ -1578,3 +1578,95 @@ export default App;
 //     </>  
 //   )
 // }
+
+const DataContext = createContext();
+
+function DataProvider({children}) {
+
+  const [data, setData] = useState([]);
+  const [skip, setSkip] = useState(0);
+
+  useEffect(() => {
+    setData([...data, Math.random()]);
+  }, [skip])
+
+  const value = {data, setData, skip, setSkip}
+
+  return (
+    <DataContext.Provider value={value}>
+      {children}
+    </DataContext.Provider>  
+  )
+}
+
+function Home() {
+  return <h1>Home</h1>
+}
+
+function Posts() {
+  const {data, setData, skip, setSkip} = useContext(DataContext);
+
+  console.log(data);
+
+  const list = data.map(item => <li key={item}>{item}</li>)
+
+  return (
+    <>
+      <h1>Posts</h1>
+      <ul>
+        {list}
+      </ul>
+      <button onClick={() => setSkip(skip + 1)}>Load more</button>
+    </>  
+  )
+}
+
+function Post() {
+  const params = useParams();
+  const postId = params.postId;
+
+  return (
+    <>
+      <h1>Post</h1>
+      <p>{postId}</p>
+    </>  
+  )
+}
+
+function Contact() {
+  return <h1>Contact</h1>
+}
+
+function NotFound() {
+  return <h1>404 NotFound</h1>
+}
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <ul> 
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <DataProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/post/:postId" element={<Post />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </DataProvider>
+    </Router>  
+  )
+}
+
