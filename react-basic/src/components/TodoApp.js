fddee505ab3634e9a6c59ede530aacb68ac5b84f
tvvmvn <tvvmvn@gmail.com>
2023-01-26 16:40:41 +0900

    1/26

--- react-basic/src/components/TodoApp.js
+++ react-basic/src/components/TodoApp.js
@@ -1,108 +1,155 @@
-import {useState} from 'react'
+import {useState, useEffect, useRef} from 'react'
 
-const initialBeers = [
-  {id: "b1", name: "Heineken", available: true},
-  {id: "b2", name: "Guinness", available: false},
-  {id: "b3", name: "Kloud", available: true}
+/*
+  INSTRUCTIONS
+  *** Before start, install and configure tailwindcss ***
+
+  1 Declare Components 
+  App, Form, FilterButton, Todo
+  
+  2 Build basic shape and list initial tasks.
+  App, Todo
+
+  3 To add todo
+  App, Form
+
+  4 To delete todo
+  App, Todo
+  
+  5 To toggle todo status
+  App, Todo
+
+  6 To filter todos
+  App, FilterButton
+
+  7 To edit todo
+  App, Todo
+*/
+
+const initialTasks = [
+  {id: "todo-0", name: "Eat", completed: true},
+  {id: "todo-1", name: "Sleep", completed: false},
+  {id: "todo-2", name: "Repeat", completed: false}
 ];
 
 const FILTER_MAP = {
   All: () => true,
-  Available: (beer) => beer.available,
-  Unavailable: (beer) => !beer.available
+  Done: (task) => task.completed,
+  Active: (task) => !task.completed
 }
 
 const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 export default function App() {
 
-  const [beers, setBeers] = useState(initialBeers);
+  const [tasks, setTasks] = useState(initialTasks);
   const [filter, setFilter] = useState('All');
   
-  console.log(beers);
+  console.log(tasks);
 
-  function addBeer(name) {
-    const newBeer = {id: Math.random(), name, available: true};
+  function addTask(name) {
+    const newTask = {id: `todo-${Math.random()}`, name, completed: false};
 
-    const updatedBeers = [...beers, newBeer];
-    setBeers(updatedBeers);
+    const updatedTasks = [...tasks, newTask];
+    setTasks(updatedTasks);
   }
 
-  function editBeer(beerId) {
-    const editedBeers = beers.map(beer => {
-      if (beer.id===beerId) {
-        return {...beer, available: !beer.available}
-      }
-      return beer;
-    })
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => task.id !== id);
 
-    setBeers(editedBeers);
+    setTasks(remainingTasks);
   }
 
-  function deleteBeer(beerId) {
-    const updatedBeers = beers.filter(beer => {
-      if (beer.id!==beerId) {
-        return beer;
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      if (task.id === id) {
+        return {...task, completed: !task.completed}
       }
+      return task;
     })
 
-    setBeers(updatedBeers);
+    setTasks(updatedTasks);
+  }
+
+  function editTask(id, newName) {
+    const editedTasks = tasks.map(task => {
+      if (task.id === id) {
+        return {...task, name: newName}
+      }
+      return task;
+    })
+
+    setTasks(editedTasks);
   }
 
   const filterButtons = FILTER_NAMES.map(name => (
     <FilterButton 
       key={name}
-      name={name} 
+      name={name}
+      isPressed={filter === name} 
       setFilter={setFilter}
     />  
   ))
 
-  const beerList = beers.filter(FILTER_MAP[filter]).map(beer => (
-    <Beer 
-      key={beer.id} 
-      beer={beer} 
-      editBeer={editBeer}
-      deleteBeer={deleteBeer}
+  const taskList = tasks.filter(FILTER_MAP[filter]).map(task => (
+    <Todo
+      key={task.id} 
+      id={task.id} 
+      name={task.name}
+      completed={task.completed}
+      deleteTask={deleteTask}
+      toggleTaskCompleted={toggleTaskCompleted}
+      editTask={editTask}
     /> 
   ))
 
+  // document
+  //   .body
+  //   .style
+  //   .backgroundColor = '#f1f1f1';
+
   return (
-    <>
-      <h1>Beers</h1>
-      <Form addBeer={addBeer} />
-      {filterButtons}
+    <div className="max-w-sm mx-auto mt-8 border p-4 bg-white">
+      <h1 className="text-2xl text-center mb-4">Todo List &#128526; &#127928;</h1>
+      <Form 
+        addTask={addTask} 
+      />
+      <div className="flex flex-nowrap gap-1 mb-4">
+        {filterButtons}
+      </div>
+      <h2 className="text-xl mb-4">{taskList.length} task(s) remaining</h2>
       <ul>
-        {beerList}
+        {taskList}
       </ul>
-    </>
+    </div>
   )
 }
 
-function Form({addBeer}) {
+function Form(props) {
   const [name, setName] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
-    addBeer(name);
+    props.addTask(name);
     setName("");
   }
 
-  function handleChange(e) {
-    const name = e.target.value;
-    setName(name);
-  }
-
   return (
-    <form onSubmit={handleSubmit}>
+    <form 
+      onSubmit={handleSubmit}
+      className="mb-4"
+    >
       <input
         type="text"
-        placeholder="Guinness"
-        onChange={handleChange}
+        className="border px-2 py-1 w-full mb-2"
         value={name}
+        onChange={(e) => setName(e.target.value)}
+        autoComplete="off"
       />
       <button
         type="submit"
-        disabled={!name}
+        className="p-1 w-full border disabled:opacity-50 text-blue-500"
+        disabled={!name.trim()}
       >
         Add
       </button>
@@ -110,27 +157,101 @@ function Form({addBeer}) {
   )
 }
 
-function FilterButton({name, setFilter}) {
+function FilterButton(props) {
 
   return (
-    <button onClick={() => setFilter(name)}>
-      {name}
+    <button 
+      className={"p-1 w-1/3 border " + (props.isPressed && "outline") }
+      onClick={() => props.setFilter(props.name)}
+    >
+      {props.name}
     </button>
   )
 }
 
-function Beer({beer, editBeer, deleteBeer}) {
-  return (
-    <li>
-      {beer.name} 
-      <div>
-        <button onClick={() => deleteBeer(beer.id)}>
+function Todo(props) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newName, setNewName] = useState("");
+  const inputEl = useRef(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.editTask(props.id, newName);
+    setIsEditing(false)
+    setNewName("")
+  }
+
+  useEffect(() => {
+    if (isEditing) {
+      inputEl.current.focus();
+    }
+  })
+
+  const viewTemplate = (
+    <>
+      <div className="flex mb-2">
+        <label>
+          <input
+            type="checkbox"
+            className="peer hidden"
+            checked={props.completed}
+            onChange={() => props.toggleTaskCompleted(props.id)}
+          />
+          <span className="text-xl peer-checked:line-through">
+            {props.name}
+          </span>
+        </label>
+      </div>
+      <div className="flex flex-nowrap gap-1">
+        <button 
+          className="w-1/2 p-1 border"
+          onClick={() => setIsEditing(true)}
+        >
+          Edit
+        </button>
+        <button
+           className="w-1/2 p-1 border text-red-500"
+           onClick={() => props.deleteTask(props.id)}
+          >
           Delete
         </button>
-        <button onClick={() => editBeer(beer.id)}>
-          {beer.available ? "Available" : "Unavailable"}
-        </button>
       </div>
-    </li>   
+    </>
+  );
+
+  const editingTemplate = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="border px-2 py-1 w-full mb-2"
+        value={newName}
+        onChange={(e) => setNewName(e.target.value)}
+        ref={inputEl}
+      />
+      <div>
+        <div className="flex flex-nowrap gap-1">
+          <button
+            type="button"
+            className="w-1/2 p-1 border"
+            onClick={() => setIsEditing(false)}
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="w-1/2 p-1 border disabled:opacity-50 text-blue-500"
+            disabled={!newName.trim()}
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    </form>
+  )
+
+  return (
+    <li className="mb-4">
+      {isEditing ? editingTemplate : viewTemplate}
+    </li>
   )
 }
