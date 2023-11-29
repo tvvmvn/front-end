99cc5bd0e430516cf969637f0d56f255f8ec61b5
tvvmvn <tvvmvn@gmail.com>
2023-11-29 15:19:36 +0900

    11/29

--- components/js/image/carousel/script.js
+++ components/js/image/carousel/script.js
@@ -2,55 +2,33 @@
 var frame = document.getElementById('frame');
 var container = document.getElementById('container');
 var images = document.getElementsByClassName("image");
-var imageWidth = 200
+var imageWidth = 200;
 // buttons
-var prev = document.getElementById('prev');
-var next = document.getElementById('next');
+var prevBtn = document.getElementById('prevBtn');
+var nextBtn = document.getElementById('nextBtn');
 // indicator
 var dots = document.getElementsByClassName('dot');
 // index and left
 var index = 0;
 var firstIndex = 0;
 var lastIndex = images.length - 1;
-var previousIndex = 0;
-var left = 0
+var left = 0;
 // touch stuff
 var x1, x2 = 0;
 var turnOverPoint = 50;
 
 
-function turnOver(data) {
-  index += data;
-  
-  left = -(index * imageWidth);
-
-  // images
-  container.style.transform = `translateX(${left}px)`;
-
-  // button
-  if (index === firstIndex) {
-    prev.classList.add('hidden');
-  } else {
-    prev.classList.remove('hidden');
-  }
-
-  if (index === lastIndex) {
-    next.classList.add('hidden');
-  } else {
-    next.classList.remove('hidden');
-  }
-
-  // indicator
-  dots[previousIndex].classList.remove('active');
-  dots[index].classList.add('active');
-
-  previousIndex = index;
-}
-
 // Touch Event
+prevBtn.addEventListener("click", function () {
+  clickHandler(-1)
+});
+nextBtn.addEventListener("click", function () {
+  clickHandler(1)
+});
 frame.addEventListener('touchstart', touchStartHandler)
 frame.addEventListener('touchmove', touchMoveHandler)
 frame.addEventListener('touchend', touchEndHandler)
+
 
 function touchStartHandler(e) {
   console.log("touch start");
@@ -60,7 +38,7 @@ function touchStartHandler(e) {
 }
 
 function touchMoveHandler(e) {
-  console.log('touch move')
+  console.log('touch move');
 
   // touching point
   x2 = e.touches[0].clientX;
@@ -69,8 +47,8 @@ function touchMoveHandler(e) {
   var movingToRight = x2 - x1 > 0;
   var movingToLeft = x2 - x1 < 0;
 
-  if (index === firstIndex && movingToRight) return;
-  if (index === lastIndex && movingToLeft) return;
+  if (index == firstIndex && movingToRight) return;
+  if (index == lastIndex && movingToLeft) return;
 
   // moving with finger
   container.style.transform = `translateX(${left + (x2 - x1)}px)`;
@@ -80,10 +58,10 @@ function touchEndHandler() {
   console.log('touch end');
 
   // no touchmove 
-  if (!x2) return;
+  if (x2 == 0) return;
 
   // check x2 and x1
-  console.log(x2, x1)
+  console.log(x2, x1);
 
   // check drawing value
   var drawEnoughToNext = -(x2 - x1) > turnOverPoint;
@@ -91,17 +69,61 @@ function touchEndHandler() {
   
   // get to prev
   if (index > firstIndex && drawEnoughToPrev) { 
-    turnOver(-1)
-  } 
-  
-  // get to next
-  if (index < lastIndex && drawEnoughToNext) { 
-    turnOver(1) 
-  } 
+    clickHandler(-1);
+    // get to next
+  } else if (index < lastIndex && drawEnoughToNext) { 
+    clickHandler(1);
+    // get back to present photo.
+  } else {
+    clickHandler(0);
+  }
     
-  // get back to present photo.
-  container.style.transform = `translateX(${left}px)`;
+  // initialize x1, x2
+  x1, x2 = 0;
+}
 
-  // initialize touching point (in case of no touchmove)
-  x2 = 0;
+function clickHandler(data) {
+  index += data;
+  left = -(index * imageWidth);
+
+  console.log("index:", index);
+  console.log("left:", left);
+
+  // image
+  setImage();
+  // buttons
+  togglePrevBtn();
+  toggleNextBtn();
+  // indicator
+  setIndicator();
+}
+
+function setImage() {
+  container.style.transform = `translateX(${left}px)`;
+}
+
+function togglePrevBtn() {
+  if (index == firstIndex) {
+    prevBtn.classList.add('hidden');
+  } else {
+    prevBtn.classList.remove('hidden');
+  }
+}
+
+function toggleNextBtn() {
+  if (index == lastIndex) {
+    nextBtn.classList.add('hidden');
+  } else {
+    nextBtn.classList.remove('hidden');
+  }
+}
+
+function setIndicator() {
+  for (var i=0; i<dots.length; i++) {
+    if (i === index) {
+      dots[i].classList.add('active');
+    } else {
+      dots[i].classList.remove('active');
+    }
+  }
 }
